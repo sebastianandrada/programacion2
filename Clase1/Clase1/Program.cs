42c97a19d8ba2345cd1204c0e8a22a4b13a58024
sebastianandrada <andrada.sebastian93@gmail.com>
2019-03-20 21:01:39 -0300

    Clase 1

---
+++ Clase1/Clase1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 1";
+            int numeroIngresado;
+            int min = int.MaxValue;
+            int maximo = int.MinValue;
+            int promedio;
+            int total = 0;
+            Console.WriteLine("Ingrese 5 numeros");
+            for(int i = 0; i < 5; i++){
+                //if(int.TryParse(Console.ReadLine(), out numeroIngresado))
+                string a = Console.ReadLine();
+                if(int.TryParse(a, out numeroIngresado)){
+                    total += numeroIngresado;
+                    if (numeroIngresado < min)
+                    {
+                        min = numeroIngresado;
+                    }else if(numeroIngresado > maximo)
+                    {
+                        maximo = numeroIngresado;
+                    }
+                }
+                
+            }
+            promedio = total / 5;
+            Console.WriteLine("el maximo es: {0}", maximo);
+            Console.WriteLine("el minimo es: {0}", min);
+            Console.WriteLine("el promedio es: {0}", promedio);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase1/Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio n° 2";
+            string aux = Console.ReadLine();
+            int numeroIngresado;
+            if (int.TryParse(aux, out numeroIngresado) && numeroIngresado > 0)
+            {
+                double cuadrado = Math.Pow(numeroIngresado, 2);
+                double cubo = Math.Pow(numeroIngresado, 3);
+                Console.WriteLine("El cuadrado es {0}", cuadrado);
+                Console.WriteLine("El cubo es {0}", cubo);
+            } else {
+                Console.WriteLine("ERROR. Reingrese numero!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
