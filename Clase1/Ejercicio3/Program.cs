a7c0165f93c0207dc8cefdcea859eb40d81b2f75
sebastianandrada <andrada.sebastian93@gmail.com>
2019-03-20 22:01:41 -0300

    clase 1 ejercicio 3

---
+++ Clase1/Ejercicio3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int numeroIngresado;
+            Console.WriteLine("Ingrese un numero");
+            string aux = Console.ReadLine();
+            if (int.TryParse(aux, out numeroIngresado)) {
+                Console.WriteLine("Calculando primos");
+                do
+                {
+                    if (esPrimo(num)){
+                        Console.WriteLine(num);
+                    }
+                    num++;
+                } while (num != numeroIngresado);
+                Console.ReadKey();
+            }
+            
+        }
+
+        private static bool esPrimo(int num)
+        {
+            bool esPrimo = true;
+            for(int i = 1; i<num; i++){
+                if(num % i != 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            return esPrimo;
+        }
+    }
+}
