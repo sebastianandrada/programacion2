78f6bf1a16df5019d0fc775202671a659b224d00
sebastianandrada <andrada.sebastian@gmail.com>
2019-05-22 20:04:59 -0300

    generics

---
+++ Ejercicios/Generics/Entidades/Torneo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Torneo<T> where T : Equipo
+    {
+        private List<T> equipos;
+        private string nombre;
+
+        public static bool operator ==(Torneo<T> torneo, Equipo equipo){
+            return true;
+        }
+
+        public static bool operator !=(Torneo<T> torneo, Equipo equipo)
+        {
+            return true;
+        }
+    }
+}
