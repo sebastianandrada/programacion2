e4501fe23821dcd54ab868a1de020f6baabcb70b
sebastianandrada <andrada.sebastian93@gmail.com>
2019-06-05 20:40:22 -0300

    clase 5-06, ejercicio 57

---
+++ Ejercicio57/Ejercicio57/Persona.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio57
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+        }
+
+        public Persona() { }
+
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            
+        }
+
+        public static void Guardar(Persona p)
+        {
+            XmlTextWriter w = new XmlTextWriter("D:\\VisualStudio\\andrada-sebastian\\Ejercicio57\\xmltest.xml", Encoding.UTF8);
+            XmlSerializer ser = new XmlSerializer(typeof(Persona));
+            //w.WriteStartElement("Persona");
+            //w.WriteString()
+            ser.Serialize(w, p);
+
+            w.Close();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Nombre: {0}, apellido: {1}", this.nombre, this.apellido);
+        }
+    }
+}
---
+++ Ejercicio57/Ejercicio57/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                XmlTextReader reader = new XmlTextReader("D:\\VisualStudio\\andrada-sebastian\\Ejercicio57\\xmltest.xml");
+                XmlSerializer ser = new XmlSerializer(typeof(Persona));
+
+                Persona persona = new Persona("sebastian", "andrada");
+                Persona.Guardar(persona);
+
+                Persona nuevaPersona;
+                nuevaPersona = (Persona)ser.Deserialize(reader);
+
+                Console.WriteLine(nuevaPersona.ToString());
+
+                reader.Close();
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
